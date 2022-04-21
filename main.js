2c9696c8543b390a1860ea908eaffd9b8da231a5
tyt101 <1104427708@qq.com>
2022-04-21 19:24:38 +0800

    轮播

--- main.js
+++ main.js
@@ -2,7 +2,25 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+// 请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中.....'
+  })
+}
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
+uni.$showMsg = function(title='数据请求失败！',duration=1500){
+  uni.showToast({
+    title:title,
+    duration:duration,
+    icon:'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <view>
-    Home
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="sl in swiperList" :key="sl.goods_id">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+sl.goods_id">
+          <image :src="sl.image_src" alt="" />
+        </navigator>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +14,31 @@
   export default {
     data() {
       return {
-        
+        swiperList:[]
       };
+    },
+    onLoad() {
+      this.getSwiperList();
+    },
+    methods:{
+      async getSwiperList(){
+        const {data:res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+        console.log(res)
+        if(res.meta.status!==200) return uni.$showMsg()
+        this.swiperList = res.message
+        uni.$showMsg('数据请求成功！')
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+swiper{
+  height: 330rpx;
+  .swiper-item,
+  image{
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
